8d9cd2752556982129081730b33fcd8d8e25e845
VishnuSandeep <73769285+VishnuSandeep@users.noreply.github.com>
2021-01-02 14:43:25 +0530

    Update zigzag.py

--- chapter3/zigzag.py
+++ chapter3/zigzag.py
@@ -1,16 +1,22 @@
 import sys, time
 indent = 0
-indentincreases = True   
-while True:
-    print(' '*indent + '******')
+indentincreases = True 
+try:
+    while True:
+         print(' '*indent + '******')
     time.sleep(0.1)
     if indentincreases:
         indent = indent + 1
         if indent == 20:
             indentincreases = False
-    else:
+     else:
         indent = indent - 1
         if indent == 0:
             indentincreases = True
+except KeyboardInterrupt:
+    sys.exit()
+       
+        
+  
 
 
